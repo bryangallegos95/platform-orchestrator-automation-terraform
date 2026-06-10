9b742c66cf42c2a21baf753629c108b5b8ca3466
Jhon Loza <jloza@bancointernacional.ec>
2026-06-09 21:05:34 -0500

    Update localst.tf

--- modules/centralized-endpoints/localst.tf
+++ modules/centralized-endpoints/localst.tf
@@ -1,1 +1,10 @@
-#to be updated
+locals {
+  region_short = var.aws_region == "us-east-1" ? "ue1" : "ue2"
+
+  tags = {
+    Aplicacion         = var.aplicacion
+    PropietarioRecurso = var.propietario_recurso
+    Producto           = var.producto
+    CentroCosto        = var.centro_costo
+  }
+}
